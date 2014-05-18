29c83a9eecee71d8de27296e8ea6a3f0f757517c
Matt Russell <mattr@netsight.co.uk>
2014-05-18 14:55:33 +0100

    Updated code to retrieve the shadow tree root node from site local utility. Moved subscriber declarations into ZCML since we're declaring utilities and patches already. Provided basic docstrings.

--- src/experimental/securityindexing/subscribers.py
+++ src/experimental/securityindexing/subscribers.py
@@ -1,32 +1,32 @@
-from Products.CMFCore.interfaces import IContentish
-from five import grok
 from plone import api
-from zope.lifecycleevent.interfaces import (
-    IObjectAddedEvent, IObjectRemovedEvent
-)
 
-from .interfaces import IShadowTree
+from .interfaces import IShadowTreeTool
 
 
 def _shadowtree_node_for_content(obj):
-    # shadowtree = component.getUtility(IShadowTre e)
     portal = api.portal.get()
     site = portal.getSiteManager()
-    shadowtree = site.getUtility(IShadowTree)
-    root = shadowtree.root
-    node = root.ensure_ancestry_to(obj)
-    return node
+    root = site.getUtility(IShadowTreeTool).root
+    return root.ensure_ancestry_to(obj)
 
 
-@grok.subscribe(IContentish, IObjectAddedEvent)
 def on_object_added(obj, event):
+    """Synchronise shadowtree security info for corresponding content.
+
+    :param obj: The content object.
+    :param event: The event.
+    """
     node = _shadowtree_node_for_content(obj)
     node.update_security_info(obj)
     assert node.physical_path == obj.getPhysicalPath()
 
 
-@grok.subscribe(IContentish, IObjectRemovedEvent)
 def on_object_removed(obj, event):
+    """Synchronise shadowtree security info for corresponding content.
+
+    :param obj: The content object.
+    :param event: The event.
+    """
     node = _shadowtree_node_for_content(obj)
     parent = node.__parent__
     if parent is not None and obj.getId() in parent:
