4e8bf76d1e5273bfb1529791f688a26b1080eab5
Matt Russell <mattr@netsight.co.uk>
2014-05-19 08:41:50 +0100

    Register the adapter used in patches such that it can be overriden for producing timings whilst installed on a site.

--- src/experimental/securityindexing/patches.py
+++ src/experimental/securityindexing/patches.py
@@ -2,8 +2,14 @@ from Products.Archetypes.config import TOOL_NAME
 from Products.Archetypes.utils import isFactoryContained
 from Products.CMFCore.interfaces import ICatalogTool
 from Products.CMFCore.utils import getToolByName
+from zope import component
 
-from .adapters import ObjectSecurity
+from .interfaces import IObjectSecurity
+
+
+def _reindex(obj, catalog):
+    adapter = component.getMultiAdapter((obj, catalog), IObjectSecurity)
+    adapter.reindex()
 
 
 def at_reindexObjectSecurity(self):
@@ -15,13 +21,11 @@ def at_reindexObjectSecurity(self):
     catalogs = [c for c in at.getCatalogsByType(self.meta_type)
                 if ICatalogTool.providedBy(c)]
     for catalog in catalogs:
-        adapter = ObjectSecurity(self, catalog)
-        adapter.reindex()
+        _reindex(self, catalog)
 
 
 def dx_reindexObjectSecurity(self):
     catalog_tool = self._getCatalogTool()
     if catalog_tool is None:  # pragma: no cover
         return
-    adapter = ObjectSecurity(self, catalog_tool)
-    adapter.reindex()
+    _reindex(self, catalog_tool)
